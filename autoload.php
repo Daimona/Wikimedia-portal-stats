8a61d25723c649cd56b834b16b0287bba1ff693a
Valerio Bozzolan <bozzolan.valerio@educ.di.unito.it>
2017-08-04 12:41:42 +0200

    -typo

--- autoload.php
+++ autoload.php
@@ -54,7 +54,7 @@ defined('D3_PATH')
 defined('JQUERY_PATH')
 	or define('JQUERY_PATH', ROOT . '/static/jquery/jquery.min.js');
 
-define('REPO_URL')
+defined('REPO_URL')
 	or define('REPO_URL', 'https://github.com/valerio-bozzolan/Wikimedia-portal-stats');
 
 // Some functions
